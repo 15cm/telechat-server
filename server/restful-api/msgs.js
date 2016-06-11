d93add5dba9a8a2208b2df6d4f097a3853f1b64c
Sinkerine <git@15cm.net>
2016-06-11 18:09:11 +0800

    Beta 0.21: Fix bug with chat msgs order

--- server/restful-api/msgs.js
+++ server/restful-api/msgs.js
@@ -5,11 +5,27 @@ import Msgs from '../models/msgs'
 
 var router = restfulMongoose(Msgs)
 // Get msg by sender_id and recver_id
-router.get('/:sid/:rid', (req, res) => {
+router.get('/and/:sid/:rid', (req, res) => {
   Msgs.find({
     sid: req.params.sid,
     rid: req.params.rid
-  }).sort('-time').exec(toRes(res))
+  }).sort('time').exec(toRes(res))
+})
+
+// Get msg by sender_id or recver_id
+router.get('/or/:sid/:rid', (req, res) => {
+  Msgs.find({
+    $or: [
+      {
+        sid: req.params.sid,
+        rid: req.params.rid
+      },
+      {
+        sid: req.params.rid,
+        rid: req.params.sid
+      }
+    ]
+  }).sort('time').exec(toRes(res))
 })
 
 // Delete msg by sender_id
